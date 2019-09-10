5b0ad3a57976b022a9d2b61e9a920187df4ffb81
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2019-09-10 14:33:35 +0530

    commented

--- stringrotation.cpp
+++ stringrotation.cpp
@@ -8,11 +8,11 @@ int main(void)
 	cout<<"Enter a string"<<endl;
 	cin>>str;
 	int l=str.length();
-	string arr[l];
-	arr[0]=str;
+	string arr[l];                // creation of array of same length as the length of string inputted
+	arr[0]=str;                   //Setting the first rotation as the string inputted
 	for(int i=1;i<l;i++)
 	{
-		string temp=arr[i-1];
+		string temp=arr[i-1];             //rotating every string by 1 place in comparison to the previous string.
 		char ch=temp[0];
 		for(int j=0;j<l-1;j++)
 		{
@@ -21,7 +21,7 @@ int main(void)
 		temp[l-1]=ch;
 		arr[i]=temp;
 	}
-	for(int i=1;i<l;i++)
+	for(int i=1;i<l;i++)               //printing all rotation but the original string
 	{
 		cout<<arr[i]<<"\t";
 	}
